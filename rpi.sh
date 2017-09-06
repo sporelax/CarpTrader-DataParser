c83ac43759e0d863856259e01adae09d65f78e0d
sporelax <perik911@gmail.com>
2017-09-06 21:37:33 +0200

    updated rpi.sh slightly

--- rpi.sh
+++ rpi.sh
@@ -5,5 +5,5 @@ cd ~pi/Git/CarpTrader-DataParser/
 git pull >> info.log 2>>info.log
 npm run exec
 git add -A >> info.log 2>>info.log
-git commit -m "daily RPI parse" >> info.log 2>> info.log
+git commit -m "daily RPI parse"
 git push
